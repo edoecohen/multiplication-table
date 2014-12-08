d6b87351e7a3e0ccb1889abe99e9099c0e2cdd15
Edoe Cohen <edoecohen@gmail.com>
2014-12-08 15:01:32 -0800

    Ability to set limit of table

--- myApp.js
+++ myApp.js
@@ -1,5 +1,20 @@
 angular.module('myApp', [])
-	.constant('VERSION', 1.1)
-    .controller('MyCtrl', function(VERSION, $scope) {
-        $scope.version = VERSION;
+    .controller('MultiplicationCtrl', function($scope, $attrs) {
+        function populateNumbers(x) {
+            var numbers = [];
+            for(var i=0; i<x; i++) {
+                numbers[i] = i + 1; 
+            };
+            return numbers;
+        }
+
+        $scope.compute = function(a,b) {
+        	return a * b;
+    	};
+
+    	$scope.$watch('numberLimit', function(limit) {
+    		$scope.numbers = populateNumbers(limit);
+    	});
+
+    	$scope.numberLimit = $attrs.initialNumberLimit || 10;
     });
